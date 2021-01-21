f8e6850d34be341f2828be417832ec871d379187
IntellectualChar <32260794+IntellectualChar@users.noreply.github.com>
2021-01-21 16:23:01 +0000

    Addition to receive string from slave

--- SPI_master.ino
+++ SPI_master.ino
@@ -1,7 +1,10 @@
 #include "SPI.h"
 char buff[]="Hello Slave\n"; //test message to send to slave
+char buff2[]="Hi i want info\n";
+char returnbyte;
 
 void setup() {
+ Serial.begin(115200);
  SPI.begin(18, 19, 23, 05);  //setting SPI pins
  SPI.setClockDivider(SPI_CLOCK_DIV8);
  pinMode(05, OUTPUT); //setting pin 5 as output
@@ -10,10 +13,14 @@ void setup() {
 
 void loop() {
  digitalWrite(05, LOW); 
- for(int i=0; i<sizeof buff; i++)
+ for(int i=0; i<sizeof buff2; i++)
  {
-  SPI.transfer(buff[i]); //send the message to the slave
+   returnbyte = SPI.transfer(buff2[i]);   //SPI.transfer(buff[i]); //send the message to the slave
+   Serial.print(returnbyte);
+   //Serial.println('\n');
  }
+
  digitalWrite(05, HIGH);
+ 
  delay(1000);  
 }
